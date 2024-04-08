c3546cd52fa8e54001b5aa3c187a22548bb16122
shyeon0602 <july8560@naver.com>
2024-04-08 13:40:54 +0900

    20240208 1340

--- 202404/20240405.c
+++ 202404/20240405.c
@@ -40,7 +40,7 @@ void BackjunPractice() {
 
 void conditionalStatement() {
 	//if문, else if문
-	
+
 	/*
 	예제
 	: 돈을 입력받고, 커피 또는 빵을 살지, 빵 구매여부를 입력받고 결과를 출력
@@ -66,7 +66,7 @@ void conditionalStatement() {
 
 	int bread;
 	char bread_input;
-	
+
 	printf("\n빵 구매여부 입력(T/F) : ");
 	/*
 	scanf_s() 함수는 사용자가 엔터 키를 입력할 때마다 개행 문자가 버퍼에 남게 되는데, 이는 다음의 scanf_s() 호출에서 문제를 일으킵니다.
@@ -152,7 +152,10 @@ void BackjunPractice2() {
 	같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
 	같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
 	모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
-	예를 들어, 3개의 눈 3, 3, 6이 주어지면 상금은 1,000+3×100으로 계산되어 1,300원을 받게 된다. 또 3개의 눈이 2, 2, 2로 주어지면 10,000+2×1,000 으로 계산되어 12,000원을 받게 된다. 3개의 눈이 6, 2, 5로 주어지면 그중 가장 큰 값이 6이므로 6×100으로 계산되어 600원을 상금으로 받게 된다.
+
+	예를 들어, 3개의 눈 3, 3, 6이 주어지면 상금은 1,000+3×100으로 계산되어 1,300원을 받게 된다.
+	또 3개의 눈이 2, 2, 2로 주어지면 10,000+2×1,000 으로 계산되어 12,000원을 받게 된다.
+	3개의 눈이 6, 2, 5로 주어지면 그중 가장 큰 값이 6이므로 6×100으로 계산되어 600원을 상금으로 받게 된다.
 
 	3개 주사위의 나온 눈이 주어질 때, 상금을 계산하는 프로그램을 작성 하시오.
 
@@ -171,7 +174,7 @@ void BackjunPractice2() {
 		money = 10000 + dice1 * 1000;
 		printf("%d", money);
 	}
-	else if((dice1 != dice2) && (dice1 != dice3) && (dice2 != dice3)) {
+	else if ((dice1 != dice2) && (dice1 != dice3) && (dice2 != dice3)) {
 		int max = 0;
 		max = ((dice1 > dice2) && (dice1 > dice3)) ? dice1 : ((dice2 > dice1) && (dice2 > dice3)) ? dice2 : dice3;
 		money = max * 100;
@@ -179,7 +182,7 @@ void BackjunPractice2() {
 	}
 	else {
 		int same = 0;
-		same = ((dice1 == dice2) || (dice1 == dice3) && (dice2 != dice3)) ? dice1 : dice2 ;
+		same = ((dice1 == dice2) || (dice1 == dice3) && (dice2 != dice3)) ? dice1 : dice2;
 		money = 1000 + same * 100;
 		printf("%d", money);
 	}
@@ -190,5 +193,5 @@ void main() {
 	//BackjunPractice();
 	//conditionalStatement();
 	//practice();
-	BackjunPractice2();
+	//BackjunPractice2();
 }
---
+++ 202404/20240408.c
@@ -0,0 +1,213 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void BackjunPractice1() {
+	/*
+	두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+	입력
+	첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+	각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+	출력
+	각 테스트 케이스마다 A+B를 출력한다.
+	*/
+	int A, B, T;
+	scanf("%d", &T);
+
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%d %d", &A, &B);
+		int sum = A + B;
+		printf("%d \n", sum);
+	}
+	
+}
+
+void BackjunPractice2() {
+	/*
+	n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+	입력
+	첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+	출력
+	1부터 n까지 합을 출력한다.
+	*/
+	int n;
+	scanf("%d", &n);
+
+	int sum = 0;
+	for (int i = 1; i <= n; i++) sum += i;
+	printf("%d", sum);
+
+}
+
+void review() {
+	// ASCII code: 인간과 컴퓨터 소통을 위하여 문자 정보를 특정 숫자와 일대일 대응시킨 약속
+
+	// 예제: 영문자를 입력받고, 대소문자 반전하여 출력하기
+	char alphabet = 0;
+	printf("영문자를 입력하세요: ");
+	scanf_s("%c", &alphabet);
+
+	int reverse;
+	// 대문자('A'==65)를 입력받는 경우 -> 소문자 반전
+	if (alphabet >= 'A' && alphabet < 'Z') {
+		reverse = alphabet + ('a' - 'A');		// alphabet + 32;
+		printf("%c, 대문자 -> 소문자: %c", alphabet, reverse);
+	}
+
+	// 소문자('a'==97)를 입력받는 경우 -> 대문자 반전
+	else if (alphabet >= 'a' && alphabet < 'z') {
+		reverse = alphabet - ('a' - 'A');		// alphabet - 32;
+		printf("%c, 소문자 -> 대문자: %c", alphabet, reverse);
+	}
+
+	// 영문자가 아닌 문자를 입력받는 경우 -> 에러메시지 출력
+	else
+		printf("Error");
+}
+
+int SwitchPractice1() {
+	// 예제1: 값(1~3)을 입력받고, 각 경우에 따라 특정 값(임의지정)을 출력하라.
+	int num;
+	printf("1에서 3 사이의 숫자를 입력하세요: ");
+	scanf("%d", &num);
+
+	// swich문은 크고 작다를 표현할 수 없음, 동등조건만 검사 가능(범위 표현 어려움)
+	switch (num)
+	{
+	case 1:
+		printf("가위");
+		break;
+
+	case 2:
+		printf("바위");
+		break;
+
+	case 3:
+		printf("보");
+		break;
+
+	default:
+		printf("Error");
+		break;
+	}
+
+}
+
+int SwitchPractice2() {
+	/*
+	1. 시험점수를 입력받고 합격/불합격 여부를 출력하라.
+	이때 점수가 60점 이상일 경우 합격에 해당된다.
+	*/
+	int score;
+	printf("시험점수를 입력하세요: ");
+	scanf("%d", &score);
+
+	int switchScore = score / 10;
+
+	switch (switchScore)
+	{
+	case 10:
+	case 9:
+	case 8:
+	case 7:
+	case 6:
+		printf("합격\n");
+		break;
+	default:
+		printf("불합격\n");
+		break;
+	}
+
+	/*
+	// true/false는 bool type이므로 switch문 내에서 바로 연산이 되기도 함
+	bool tf = (score >= 60);
+	switch	(tf) {
+	case true:
+		printf("합격");
+		break;
+	case false:
+		printf("불합격");
+		break;
+	}
+	*/
+
+}
+
+int SwitchPractice3() {
+	/*
+	2. 성적을 입력받고, 이 성적이 어느 학점에 해당되는지를 출력하라.
+	- A: 100-90
+	- B: 89-80
+	- C: 79-70
+	- D: 69-60
+	- F
+	*/
+	int score;
+	printf("성적을 입력하세요: ");
+	scanf(" %d", &score);
+
+	int switchScore = score / 10;
+
+	switch (switchScore)
+	{
+	case 10:
+	case 9:
+		printf("A");
+		break;
+	case 8:
+		printf("B");
+		break;
+	case 7:
+		printf("C");
+		break;
+	case 6:
+		printf("D");
+		break;
+	default:
+		printf("F");
+		break;
+	}
+}
+
+int Loop() {
+	// 예제: 1-5 출력하는 프로그램
+	for (int i = 1; i <= 5; i++)
+		printf("%d", i);
+		
+	/*
+	반복문: 지정된 횟수만큼 동일한 명령문을 반복하는 구문
+		지정된 횟수만큼 => 특정 조건식에 대한 참/거짓으로 책정
+	*/
+	printf("\n");
+	// while문
+	int num = 0;
+	while (num < 5)
+	{
+		num++;
+		printf("%d", num);
+	}
+	printf("\n");
+	num = 1;
+	while (num <= 5)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+}
+
+int main() {
+	//review();
+	//SwitchPractice1();
+	//SwitchPractice2();
+	//SwitchPractice3();
+	//Loop();
+
+	//BackjunPractice1();
+	//BackjunPractice2();
+}
