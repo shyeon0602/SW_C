9fe02e2a73b339b72b61ad5232c6b080780d4fb8
shyeon0602 <july8560@naver.com>
2024-04-18 11:19:21 +0900

    20240418 1120

---
+++ 202404/20240418_2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+예제: main에서 숫자 num을 입력받고
+재귀함수를 사용해서 num에 대한 num!(팩토리얼) 값을 출력
+*/
+
+int result = 1;
+int factorial(int num) {
+	//num *= (num - 1);
+	//printf("%d", num);
+	/*
+	참고!
+	팩토리얼 규칙 중 0! 과 1!은 1로 정의되어있음
+	*/
+	if (num <= 1) return 1;		// 반환값이 1 이상의 팩토리얼 값을 반환하도록 함
+	return num * factorial(num - 1);
+}
+
+int realFactorial(int num) {
+	for (int j = 1; j <= num; j++)
+		result *= j;
+	printf("반복문 사용: %d", result);
+}
+
+int main() {
+	int i;
+	printf("숫자를 입력하세요: ");
+	scanf("%d", &i);
+
+	printf("재귀함수 사용: %d", factorial(i));
+
+	printf("\n");
+
+	realFactorial(i);
+}
